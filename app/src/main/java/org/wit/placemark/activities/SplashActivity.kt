7343f50a24547d03d6d32c7c1f3c284d39519d4c
Matthew O'Connor <Matthew@mbp>
2018-11-02 16:27:21 +0000

    splash screen completed

--- app/src/main/java/org/wit/placemark/activities/SplashActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/SplashActivity.kt
@@ -21,7 +21,7 @@ class SplashActivity : AppCompatActivity() {
 
   internal val Runnable: Runnable = Runnable {
     if (!isFinishing) {
-      val intent = Intent(applicationContext, PlacemarkListActivity::class.java)
+      val intent = Intent(this, PlacemarkListActivity::class.java)
       startActivity(intent)
       finish()
     }
