53656046ad50e3c89fda5e15c47bc4b919c2d993
Matthew O'Connor <Matthew@mbp>
2019-08-17 02:20:28 +0100

    'Firebase storage now implemented'

--- app/src/main/java/org/wit/hillfort/models/firebase/HillfortFireStore.kt
+++ app/src/main/java/org/wit/hillfort/models/firebase/HillfortFireStore.kt
@@ -1,18 +1,24 @@
 package org.wit.hillfort.models.firebase
 
-
 import android.content.Context
+import android.graphics.Bitmap
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import org.jetbrains.anko.AnkoLogger
+import org.wit.hillfort.helpers.readImageFromPath
 import org.wit.hillfort.models.HillfortModel
 import org.wit.hillfort.models.HillfortStore
+import java.io.ByteArrayOutputStream
+import java.io.File
 
 class HillfortFireStore(val context: Context) : HillfortStore, AnkoLogger {
 
   val hillforts = ArrayList<HillfortModel>()
   lateinit var userId: String
   lateinit var db: DatabaseReference
+  lateinit var st: StorageReference
 
   suspend override fun findAll(): List<HillfortModel> {
     return hillforts
@@ -25,9 +31,12 @@ class HillfortFireStore(val context: Context) : HillfortStore, AnkoLogger {
 
   suspend override fun create(hillfort: HillfortModel) {
     val key = db.child("users").child(userId).child("hillforts").push().key
-    hillfort.fbId = key!!
-    hillforts.add(hillfort)
-    db.child("users").child(userId).child("hillforts").child(key).setValue(hillfort)
+    key?.let {
+      hillfort.fbId = key
+      hillforts.add(hillfort)
+      db.child("users").child(userId).child("hillforts").child(key).setValue(hillfort)
+      updateImage(hillfort)
+    }
   }
 
   suspend override fun update(hillfort: HillfortModel) {
@@ -40,6 +49,9 @@ class HillfortFireStore(val context: Context) : HillfortStore, AnkoLogger {
     }
 
     db.child("users").child(userId).child("hillforts").child(hillfort.fbId).setValue(hillfort)
+    if ((hillfort.image.length) > 0 && (hillfort.image[0] != 'h')) {
+      updateImage(hillfort)
+    }
   }
 
   suspend override fun delete(hillfort: HillfortModel) {
@@ -47,21 +59,47 @@ class HillfortFireStore(val context: Context) : HillfortStore, AnkoLogger {
     hillforts.remove(hillfort)
   }
 
-   override fun clear() {
+  override fun clear() {
     hillforts.clear()
+  }
+
+  fun updateImage(hillfort: HillfortModel) {
+    if (hillfort.image != "") {
+      val fileName = File(hillfort.image)
+      val imageName = fileName.getName()
+
+      var imageRef = st.child(userId + '/' + imageName)
+      val baos = ByteArrayOutputStream()
+      val bitmap = readImageFromPath(context, hillfort.image)
+
+      bitmap?.let {
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val data = baos.toByteArray()
+        val uploadTask = imageRef.putBytes(data)
+        uploadTask.addOnFailureListener {
+          println(it.message)
+        }.addOnSuccessListener { taskSnapshot ->
+          taskSnapshot.metadata!!.reference!!.downloadUrl.addOnSuccessListener {
+            hillfort.image = it.toString()
+            db.child("users").child(userId).child("hillforts").child(hillfort.fbId).setValue(hillfort)
+          }
+        }
+      }
+    }
   }
 
   fun fetchHillforts(hillfortsReady: () -> Unit) {
     val valueEventListener = object : ValueEventListener {
-      override fun onCancelled(error: DatabaseError) {
+      override fun onCancelled(dataSnapshot: DatabaseError) {
       }
       override fun onDataChange(dataSnapshot: DataSnapshot) {
-        dataSnapshot.children.mapNotNullTo(hillforts) { it.getValue<HillfortModel>(HillfortModel::class.java) }
+        dataSnapshot!!.children.mapNotNullTo(hillforts) { it.getValue<HillfortModel>(HillfortModel::class.java) }
         hillfortsReady()
       }
     }
     userId = FirebaseAuth.getInstance().currentUser!!.uid
     db = FirebaseDatabase.getInstance().reference
+    st = FirebaseStorage.getInstance().reference
     hillforts.clear()
     db.child("users").child(userId).child("hillforts").addListenerForSingleValueEvent(valueEventListener)
   }
